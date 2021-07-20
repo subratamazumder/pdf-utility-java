2eed40225e4d24b568c1cf34357bd2412da5e7df
Subrata Mazumder <subrata.besu@gmail.com>
2021-07-21 01:38:41 +0530

    UT added

--- src/test/java/com/subrata/poc/pdf/PDFMainUtilTest.java
+++ src/test/java/com/subrata/poc/pdf/PDFMainUtilTest.java
@@ -1,10 +1,11 @@
 package com.subrata.poc.pdf;
 
+import com.subrata.poc.pdf.util.PDFMergerUtil;
 import org.junit.Test;
 
 import java.io.IOException;
 
-public class PDFMergerUtilTest {
+public class PDFMainUtilTest {
 
     @Test (expected = IOException.class)
     public void mergeHandleErrorIfDirIsInvalid() throws IOException{
